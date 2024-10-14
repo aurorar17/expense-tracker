2674b01f0a98450ac9a6768591149c65bc702bf4
Hyejin Leem <jin2im@gmail.com>
2024-10-14 16:37:42 -0700

    feat: save the expense-list

--- classes/Header.js
+++ classes/Header.js
@@ -1,5 +1,8 @@
-fetch("/header.html")
-            .then((response) => response.text())
-            .then((data) => {
-              document.querySelector("header").innerHTML = data;
-            });
+fetch("/components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+        document.querySelector("header").innerHTML = data;
+
+    });
+
+
