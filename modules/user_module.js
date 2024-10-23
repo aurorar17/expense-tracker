248e2ab6b0737728e3d0d970956283a0b6237e84
juniahn-dev <gud0415@gmail.com>
2024-10-22 21:10:23 -0700

    [chore] Display categories list in user page

--- modules/user_module.js
+++ modules/user_module.js
@@ -1,7 +1,22 @@
+import Category from "../../classes/Category.js";
+export const categories = new Map();
+
 const mapFiller = (data) => {
   document.querySelector("#user-name").innerHTML =
     data.fname + " " + data.lname;
   //   document.querySelector("#user-email").innerHTML = data.email;
 };
 
+const categoryMapFiller = (data) => {
+  for (let item of data) {
+    let tmpProduct = new Category(item.id, item.name, item.description);
+
+    const li = document.createElement("li");
+    li.innerHTML = `${tmpProduct.name}<br />`;
+    categories.set(tmpProduct.id, tmpProduct);
+    document.querySelector("#bankInformationSection").append(li);
+  }
+};
+
 load("http://127.0.0.1:5500/const/user_data.json", mapFiller);
+load("http://127.0.0.1:5500/const/category_data.json", categoryMapFiller);
