615972cf84c852fb32fa88156a85a0b35a9a8025
Hyejin Leem <jin2im@gmail.com>
2024-10-23 19:36:37 -0700

    feat: total limit progress bar

--- functions/main-expense-function.js
+++ functions/main-expense-function.js
@@ -10,7 +10,6 @@ function formatDate(dateStr) {
     return `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}`;
 }
 
-<<<<<<< HEAD
 // Monthly income & monthly expense -- key => selected Month from top
 function updateBalances(selectedMonthValue) {
     const expenses = JSON.parse(localStorage.getItem("expense")) || [];
@@ -49,10 +48,28 @@ function updateBalances(selectedMonthValue) {
         .filter((expense) => expense.income === "expense")
         .reduce((sum, expense) => sum + (expense.cost || 0), 0);
 
+    const monthlyTotal = currentIncome - currentExpense;    
+
     // Add the monthly income & expense in UI
     document.querySelector(".monthlyExpense .income h2").textContent = `+ $ ${currentIncome.toFixed(2)}`;
     document.querySelector(".monthlyExpense .expense h2").textContent = `- $ ${currentExpense.toFixed(2)}`;
-}
+    document.querySelector(".monthly-total").textContent = `$ ${monthlyTotal}`
+
+
+    // GET the limit data from localStorage
+    const categoriesData = localStorage.getItem('categories');
+
+    if (categoriesData) {
+      const categories = JSON.parse(categoriesData);
+      // add all limit's value
+      const totalLimit = categories.reduce((sum, category) => {
+        return sum + category.limit;
+      }, 0);
+
+      document.querySelector(".total-limit").textContent = `$ ${totalLimit}`
+    } 
+        
+    } 
 
 // Update balances on initial load
 updateBalances(currentMonth);
@@ -104,44 +121,12 @@ function updateTable(selectedMonthValue) {
         `;
         tableBody.appendChild(newRow);
     });
-=======
-function updateTable() {
-  const expenses = JSON.parse(localStorage.getItem("expense")) || [];
-  const tableBody = document.querySelector("#transactionTable tbody");
-  tableBody.innerHTML = "";
-
-  expenses.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  expenses.slice(0, 15).forEach((expense) => {
-    const newRow = document.createElement("tr");
-
-    if (expense.income === "income") {
-      newRow.classList.add("transIncome");
-      expense.cost = `<span>+ $ ${expense.cost.toFixed(2)}</span>`
-    } else if (expense.income === "expense") {
-      newRow.classList.add("transExpense");
-      expense.cost = `<span>- $ ${expense.cost.toFixed(2)}</span>`
-    }
-
-    newRow.innerHTML = `
-        <td></td>
-        <td>${formatDate(expense.date)}</td>
-        <td>${expense.category}</td>
-        <td>${expense.description}</td>
-        <td class="cost">${expense.cost}</td>
-      `;
-    tableBody.appendChild(newRow);
-  });
-
-  updateBalances(expenses);
->>>>>>> main
 }
 
 // Initial table update
 const initialSelectedMonth = parseInt(document.getElementById("currentMonthSelect").value);
 updateTable(initialSelectedMonth);
 
-<<<<<<< HEAD
 // Function to add an expense
 function addExpense() {
     fetch("/pages/add-expense/index.html")
@@ -152,53 +137,6 @@ function addExpense() {
         import("/modules/category_module.js").then(({ categories }) => {
           document.getElementById("inputForm").addEventListener("submit", (e) => {
             e.preventDefault();
-=======
-function updateBalances() {
-  const expenses = JSON.parse(localStorage.getItem("expense")) || [];
-  const now = new Date();
-  const currentMonth = now.getMonth();
-  const currentYear = now.getFullYear();
-
-  // calculate of total income & total expense for total balance
-  const totalIncome = expenses
-    .filter((expense) => expense.income === "income")
-    .reduce((sum, income) => sum + (income.cost || 0), 0);
-
-  const totalExpense = expenses
-    .filter((expense) => expense.income === "expense")
-    .reduce((sum, expense) => sum + (expense.cost || 0), 0);
-
-  const totalBalance = totalIncome - totalExpense;
-
-  // Filtering of current Month
-  const currentMonthExpenses = expenses.filter((expense) => {
-    const expenseDate = new Date(expense.date);
-    return (
-      expenseDate.getMonth() === currentMonth && expenseDate.getFullYear() === currentYear
-    );
-  });
-
-  // calculate of current Income & Expense according to Filtering
-  const currentMonthIncome = currentMonthExpenses
-    .filter((expense) => expense.income === "income")
-    .reduce((sum, income) => sum + (income.cost || 0), 0);
-
-  const currentMonthExpense = currentMonthExpenses
-    .filter((expense) => expense.income === "expense")
-    .reduce((sum, expense) => sum + (expense.cost || 0), 0);
-
-  document.querySelector(".current-box h1").textContent = `$ ${totalBalance.toFixed(2)}`;
-
-  // If we want to use the filter again, We can change the value to "currentMonthIncome" and uncomment.
-  document.querySelector(".monthlyExpense .income h2").textContent = `+ $ ${currentMonthIncome.toFixed(2)}`;
-  document.querySelector(".monthlyExpense .expense h2").textContent = `- $ ${currentMonthExpense.toFixed(2)}`;
-}
-
-
-updateBalances();
-
-updateTable();
->>>>>>> main
 
             const income = document.querySelector("#income").value;
             const category = document.querySelector("#categoryList").value;
@@ -210,7 +148,6 @@ updateTable();
 
             let expenses = JSON.parse(localStorage.getItem("expense")) || [];
 
-<<<<<<< HEAD
             const currentSpendAmount = expenses.reduce((result, expense) => {
               if (
                 expense.category === currentCategory.name &&
@@ -273,37 +210,3 @@ $(".add-expense").click(function(){
         $(".add-expense").html("+");
     }
 });
-=======
-// Mobile bottom area
-$(".add-expense").click(function () {
-  $("#mobileAddExpenseArea").toggleClass("add-popup")
-  if ($("#mobileAddExpenseArea").hasClass("add-popup")) {
-    $(".add-expense").html("&#215;");
-  } else {
-    $(".add-expense").html("+");
-  }
-})
-
-
-
-
-//user info page
-function updateBalances() {
-  // Codice per aggiornare i bilanci
-}
-
-function updateTable() {
-  // Codice per aggiornare la tabella
-}
-
-function displayMonthlySummary() {
-  // Codice per visualizzare il riepilogo mensile
-}
-
-// Assicurati che le funzioni vengano eseguite dopo il caricamento della pagina
-window.onload = function () {
-  updateBalances();
-  updateTable();
-  displayMonthlySummary();
-};
->>>>>>> main
