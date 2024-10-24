5fc53dd438ad87bd3902fdd5f8b665ad6bf1f9c2
juniahn-dev <gud0415@gmail.com>
2024-10-23 17:14:01 -0700

    [chore] Edit all things

--- classes/User.js
+++ classes/User.js
@@ -1,9 +1,10 @@
 class User {
-  constructor(uid, fname, lname, email, password) {
+  constructor(uid, fname, lname, email, phone, password) {
     this.uid = uid;
     this.fname = fname;
     this.lname = lname;
     this.email = email;
+    this.phone = phone;
     this.password = password;
   }
 }
--- modules/user_module.js
+++ modules/user_module.js
@@ -1,22 +1,48 @@
 import Category from "../../classes/Category.js";
-export const categories = new Map();
 
-const mapFiller = (data) => {
-  document.querySelector("#user-name").innerHTML =
-    data.fname + " " + data.lname;
-  //   document.querySelector("#user-email").innerHTML = data.email;
+export const userFiller = (data) => {
+  const localStorageUser = JSON.parse(localStorage.getItem("user"));
+
+  const fname = localStorageUser?.fname || data.fname;
+  const lname = localStorageUser?.lname || data.lname;
+  const email = localStorageUser?.email || data.email;
+  const phone = localStorageUser?.phone || data.phone;
+
+  document.querySelector("#user-name").innerHTML = fname + " " + lname;
+  document.querySelector("#lname").innerHTML = localStorageUser?.lname || lname;
+  document.querySelector("#fname").innerHTML = fname;
+  document.querySelector("#email").innerHTML = email;
+  document.querySelector("#phone").innerHTML = phone;
+
+  delete data.password;
+
+  localStorage.setItem("user", JSON.stringify(localStorageUser || data));
 };
 
 const categoryMapFiller = (data) => {
+  const localStorageCategories = JSON.parse(localStorage.getItem("categories"));
+  const categories = [];
+
   for (let item of data) {
-    let tmpProduct = new Category(item.id, item.name, item.description, item.limit);
+    let tmpProduct = new Category(
+      item.id,
+      item.name,
+      item.description,
+      item.limit
+    );
 
     const li = document.createElement("li");
     li.innerHTML = `<div>${tmpProduct.name}</div><div>${tmpProduct.description}</div><div>${tmpProduct.limit}</div>`;
-    categories.set(tmpProduct.id, tmpProduct);
+    const category = JSON.parse(JSON.stringify(tmpProduct));
+    categories.push(category);
     document.querySelector("#bankInformationSection").append(li);
   }
+
+  localStorage.setItem(
+    "categories",
+    JSON.stringify(localStorageCategories || categories)
+  );
 };
 
-load("http://127.0.0.1:5500/const/user_data.json", mapFiller);
+load("http://127.0.0.1:5500/const/user_data.json", userFiller);
 load("http://127.0.0.1:5500/const/category_data.json", categoryMapFiller);
